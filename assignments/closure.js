2930471a5072464c46495863f2783920435cbbd8
Megan <mennis0145@gmail.com>
2019-08-20 14:34:59 +0100

    closure added to closures.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,16 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+let x = 9;
+let y = 10;
+
+function exponent() { //console.log(exponent()); --> 180
+  function multiplynumbers() {
+    return x * y;
+  }
+  return multiplynumbers() * 2;
+}
+console.log(exponent());
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
