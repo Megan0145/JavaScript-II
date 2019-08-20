4c50ced55a2b917f16120c45dfcb25e1ae7d7b4a
Megan <mennis0145@gmail.com>
2019-08-20 15:39:41 +0100

    counter created for stretch qs in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -26,7 +26,14 @@ const counterMaker = () => {
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+  let count = 0;
+  function counter(){
+    return count +=1;
+  }
+return counter();
 };
+
+
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
